7807ffb41178f09311af318340b8f76ba356a919
admin <411684228@qq.com>
2026-08-19 16:00:19 +0800

    databasemigration

--- my-app/routes/api.php
+++ my-app/routes/api.php
@@ -1,27 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\HelloController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('/hello',function(){
-    return response()->json([
-        'message' => 'Hello World',
-        'success' => true,
-    ]);
-    
-});
+Route::get('/hello', [HelloController::class, 'index']);
