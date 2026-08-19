d691963097e3ccb1271221f36a07e83ecacab73c
admin <411684228@qq.com>
2026-08-19 15:29:11 +0800

    Route

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return ('this is my first laravel page');
 });
 
 Route::get('/hello',function(){
-   return "Hello World";
+    return "Hello World";
 });
 
