9ece13e7f26893e72449bf947e728370d069b021
admin <411684228@qq.com>
2026-08-20 16:11:16 +0800

    create require

--- my-app/app/Http/Controllers/Api/TaskController.php
+++ my-app/app/Http/Controllers/Api/TaskController.php
@@ -3,9 +3,32 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        $tasks = Task::latest()->get();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Tasks loaded successfully',
+            'data' => $tasks,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $data=$request->validate([
+            'title' => 'required|string|max:255',
+        ]);
+        $task=Task::create($data);
+        return response()->json([
+            'success' => true,
+            'message' => 'Task created successfully',
+            'data' => $task,
+        ], 201);
+    }
 }
--- my-app/routes/api.php
+++ my-app/routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\HelloController;
+use App\Http\Controllers\Api\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/hello', [HelloController::class, 'index']);
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::post('/tasks', [TaskController::class, 'store']);
